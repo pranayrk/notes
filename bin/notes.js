b82aa9cabd3ac0b1bf08d4165075a3a8172e3af3
Pranay Raja Krishnan <xpranayx@gmail.com>
2025-07-09 18:53:18 +0530

    test

--- bin/notes.js
+++ bin/notes.js
@@ -1,6 +1,28 @@
 const HOME = "https://pranayrk.github.io/notes/";
 const NOTES = HOME + "notes/";
 
+function loadReveal() {
+    Reveal.initialize({
+        width: 1240,
+        height: 760,
+        margin: 0.04,
+        hash: true,
+        plugins: [ RevealMarkdown, RevealHighlight, RevealNotes, RevealMath.KaTeX]
+    });
+}
+
+function load(content) {
+    Reveal.destroy();
+    slides = document.getElementsByClassName("slides")[0];
+    slides.innerHTML = content;
+    loadReveal();
+    Reveal.slide(0);
+}
+
+function loadMarkdown(content) {
+    load("<section data-markdown>\n" + content + "\n</section>")
+}
+
 function loadDir(dir) {
     fetch(NOTES + dir + ".dir")
         .then(res => {
@@ -119,3 +141,4 @@ function attachLinkEvent() {
         }
     });
 }
+
